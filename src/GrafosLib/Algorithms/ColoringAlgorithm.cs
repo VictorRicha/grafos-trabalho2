93d17b15db5b7979cd52949c77917571ea027a35
Victor Richa <vdabarian@inoa.com.br>
2016-02-29 22:39:51 -0300

    coloring

---
+++ src/GrafosLib/Algorithms/ColoringAlgorithm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GrafosLib.Model;
+
+namespace GrafosLib.Algorithms
+{
+    public class ColoringAlgorithm
+    {
+        private readonly IGraph _graph;
+        private int _colorCount;
+
+        public ColoringAlgorithm(IGraph graph)
+        {
+            _graph = graph;
+            _colorCount = 0;
+        }
+
+        /// <summary>
+        ///     Color the vertices in the graph using the Welsh-Powell algorithm
+        /// </summary>
+        public void RunColoring()
+        {
+            //Order the vertices in descending order according to their degree
+            var orderedVertices = _graph.Degrees().OrderByDescending(i => i.Value).Select(a => a.Key).ToList();
+            var colorMap = new int[_graph.VerticesCount + 1];
+
+            while (orderedVertices.Any())
+            {
+                var colored = new List<int>();
+                _colorCount++;
+
+                foreach (var vertex in orderedVertices)
+                {
+                    if (colorMap[vertex] == 0 && !Conflict(vertex, colored))
+                    {
+                        colored.Add(vertex);
+                        colorMap[vertex] = _colorCount;
+                    }
+                }
+
+                foreach (var c in colored)
+                {
+                    orderedVertices.Remove(c);
+                }
+            }
+
+            if (!IsValidColoring(colorMap))
+            {
+                throw new Exception("Invalid Coloring!");
+            }
+        }
+
+        private bool Conflict(int vertex, List<int> colored)
+        {
+            var n = _graph.Neighbours(vertex).Select(e => e.Target);
+            return n.Intersect(colored).Any();
+        }
+
+        public int ColorCount()
+        {
+            return _colorCount;
+        }
+
+        public bool IsValidColoring(int[]colorMap)
+        {
+            return !colorMap.Where((t, i) => _graph.Neighbours(i).Any(neighbour => colorMap[neighbour.Target] == t)).Any();
+        }
+    }
+}
--- src/GrafosLib/Program.cs
+++ src/GrafosLib/Program.cs
@@ -18,8 +18,8 @@ namespace GrafosLib
                 //     TextReader fin = new StreamReader( args[0] );
                 Console.WriteLine("Choose file");
                 var file = Console.ReadLine();
-                //TextReader fin = new StreamReader($"c:\\Grafos\\grafo_{file}.txt");
-                TextReader fin = new StreamReader($"c:\\Grafos\\rede_colaboracao.txt");
+                TextReader fin = new StreamReader($"c:\\Grafos\\G{file}.txt");
+                //TextReader fin = new StreamReader($"c:\\Grafos\\rede_colaboracao.txt");
                 Console.WriteLine($"Reading file {file}...");
                 var counter = 1;
                 string line;
@@ -35,16 +35,24 @@ namespace GrafosLib
                         }
                         else
                         {
-                            if (st.Length != 3)
+                            if (st.Length == 3)
+                            {
+                                var source = int.Parse(st[0]);
+                                var dest = int.Parse(st[1]);
+                                var culture = new CultureInfo("en");
+                                var cost = double.Parse(st[2], culture);
+                                g.AddEdge(source, dest, cost);
+
+                            }else if (st.Length == 2)
+                            {
+                                var source = int.Parse(st[0]);
+                                var dest = int.Parse(st[1]);
+                                g.AddEdge(source, dest);
+                            }
+                            else
                             {
                                 Console.Error.WriteLine("Skipping ill-formatted line " + line);
-                                continue;
                             }
-                            var source = int.Parse(st[0]);
-                            var dest = int.Parse(st[1]);
-                            CultureInfo culture = new CultureInfo("en");
-                            var cost = double.Parse(st[2], culture);
-                            g.AddEdge(source, dest, cost);
                         }
                     }
                     catch (FormatException) { Console.Error.WriteLine("Skipping ill-formatted line " + line); }
@@ -56,86 +64,17 @@ namespace GrafosLib
 
             Console.WriteLine("File read...");
             Console.WriteLine(g.VerticesCount + " vertices");
+            Console.WriteLine(g.Edges().Count()/2 + " edges");
 
             var sw = new Stopwatch();
 
             sw.Start();
 
-            //run dijkstra
-            //Console.WriteLine("Run Dijkstra");
+            var coloring = new ColoringAlgorithm(g);
 
-            //var d = new Dijkstra(g);
-            //d.Run(40);
+            coloring.RunColoring();
 
-            //sw.Stop();
-            //Console.WriteLine("End");
-            //Console.WriteLine($"Distance to vertex 1: {d.DistanceTo(1)}");
-
-            //Console.WriteLine("Run Prim");
-            //var p = new Prim(g);
-            //sw.Stop();
-
-            //Console.WriteLine($"MST weight: {p.Weight()}");
-
-            //Console.WriteLine("Calc " + DateTime.Now.ToString("O"));
-            //var avg = new AveragePathLength(g).Run();
-            //Console.WriteLine("Average Path Length: {0}", avg);
-            //sw.Stop();
-            //Console.WriteLine("Done " + DateTime.Now.ToString("O"));
-
-
-            var d = new Dijkstra(g);
-            d.Run(2722);
-
-            //sw.Stop();
-            //Console.WriteLine("End");
-            //Console.WriteLine($"Distance to turing: {d.DistanceTo(11365)}");
-            //Console.WriteLine($"Distance to kruskal: {d.DistanceTo(211706)}");
-            //Console.WriteLine($"Distance to kleinberg: {d.DistanceTo(5709)}");
-            //Console.WriteLine($"Distance to tardos: {d.DistanceTo(11386)}");
-            //Console.WriteLine($"Distance to daniel: {d.DistanceTo(343930)}");
-
-
-            var pathToDaniel = d.PathTo(11386);
-            if (pathToDaniel == null)
-            {
-                Console.WriteLine("indefinito");
-                return;
-            }
-            foreach (var edge in pathToDaniel)
-            {
-                Console.WriteLine(edge);
-            }
-
-            //Console.WriteLine("Run Prim");
-            //var p = new Prim(g);
-            //sw.Stop();
-
-            //Console.WriteLine($"MST weight: {p.Weight()}");
-            //var mstEdges = p.Edges();
-            //var mst = new AdjacencyListGraph(true);
-            //foreach (var mstEdge in mstEdges)
-            //{
-            //    mst.AddEdge(mstEdge.Source, mstEdge.Target, mstEdge.Weight);
-            //}
-
-
-            //var degrees = mst.Degrees();
-            //var d = degrees.Values.OrderByDescending(a => a).ToList();
-            //var first = degrees.Where(e => e.Value == d[0]);
-            //var second = degrees.Where(e => e.Value == d[1]);
-            //var third = degrees.Where(e => e.Value == d[2]);
-            //Console.WriteLine($"Degree 1: {first.FirstOrDefault().Key}  {d[0]}");
-            //Console.WriteLine($"Degree 2: {second.FirstOrDefault().Key} {d[1]}");
-            //Console.WriteLine($"Degree 3: {third.FirstOrDefault().Key} {d[2]}");
-
-            //Console.WriteLine(
-            //    $"Dijkstra neighbours: {mst.Neighbours(2722).Aggregate("", (current, neighbour) => current + (neighbour + " - "))}");
-            //Console.WriteLine(
-            //    $"Figueiredo neighbours: {mst.Neighbours(343930).Aggregate("", (current, neighbour) => current + (neighbour + " - "))}");
-
-
-
+            Console.WriteLine("Colors={0}", coloring.ColorCount());
             Console.WriteLine("Elapsed={0}", sw.Elapsed.TotalMilliseconds);
             Console.ReadLine();
         }
